c39944b562f4cbb1e63f802b66c32b0f926be8b7
DESKTOP-RJ3K5CJ\Le Duc Hai <leduchaif@gmail.com>
2017-05-06 17:09:55 +0700

    first commit

---
+++ leduchai.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(leduchai.Web.Startup))]
+namespace leduchai.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
